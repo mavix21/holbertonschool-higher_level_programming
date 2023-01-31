971ba8e8f7c7eeca249fc876585a7b3f6be341fd
mavix21 <marcevizcarra2109@hotmail.com>
2023-01-30 22:20:41 -0500

    Prints a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    i = 0
+    while i < len(matrix):
+        j = 0
+        while j < len(matrix[i]):
+            if j > 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[i][j]), end="")
+            j += 1
+        print()
+        i += 1
