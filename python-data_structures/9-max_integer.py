57d164ab98ab751cce8e0264f02815547309858b
mavix21 <marcevizcarra2109@hotmail.com>
2023-01-30 23:17:18 -0500

    Returns the biggest integer of a given list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max_value = my_list[0]
+    i = 1
+    while i < len(my_list):
+        if my_list[i] > max_value:
+            max_value = my_list[i]
+        i += 1
+
+    return max_value
