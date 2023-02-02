b2ec3312e9ef4a8c0d6e99a54428961970933a20
marcelo.vizcarra <marcelo.vizcarra@inventiva-solutions.com>
2023-02-02 15:23:43 -0500

    Optimized version using set and sum functions

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
-    uniq_list = []
-
-    for i in my_list:
-        if i not in uniq_list:
-            uniq_list.append(i)
-
-    for x in uniq_list:
-        sum += x
-
-    return (sum)
+    return sum(set(my_list))
