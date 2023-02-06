63435d2cca38965bb32188f0aadc2e528da8cc25
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-05 20:24:24 -0500

    Class that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines a class Square"""
+
+
+class Square:
+    """
+    A class used to represent a Square
+
+    Attributes
+    ----------
+    __size: int/float
+        The size of the square.
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
