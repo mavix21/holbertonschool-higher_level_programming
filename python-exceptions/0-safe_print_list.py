7e35c42fa7f98ebc063c6dca6cece9332373b6e9
marcelo.vizcarra <marcelo.vizcarra@inventiva-solutions.com>
2023-02-06 17:00:39 -0500

    Safely prints x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    nb_print = 0
+    elements_printed = 0
 
     try:
-        while i < x:
+        for i in range(x):
             print(my_list[i], end="")
-            i += 1
-        nb_print = x
-    except Exception:
-        nb_print = i
+            elements_printed += 1
+    except IndexError:
+        pass
 
     print()
-    return (nb_print)
+    return (elements_printed)
