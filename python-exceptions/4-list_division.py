f932eb8159ed10cee02c7340d4008a2d33317e3c
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-08 22:57:15 -0500

    Fix: result variable set to zero in finally clause

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    result = 0
     div_list = []
 
     for i in range(list_length):
@@ -7,13 +8,12 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            result = 0
         except (TypeError, ValueError):
             print("wrong type")
-            result = 0
         except IndexError:
             print("out of range")
         finally:
             div_list.append(result)
+            result = 0
 
     return div_list
