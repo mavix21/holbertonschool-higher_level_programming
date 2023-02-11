29f9a80525b64e5a6f2130e25a34958002459af3
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-11 00:57:01 -0500

    pydocstyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,27 +15,31 @@ def matrix_divided(matrix, div):
     result_matrix = []
     result_row = []
 
-    if not isinstance(matrix, (list)) or not matrix:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    for sublist in matrix:
-        if not isinstance(sublist, (list)):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        for element in sublist:
-            if type(element) != int and type(element) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    row_size = len(matrix[0])
-    if len(matrix) > 1:
-        for row in matrix[1:]:
-            if len(row) != row_size:
-                raise TypeError("Each row of the matrix (list of lists) must have the same size")
-
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
+    if not isinstance(matrix, (list)) or not matrix:
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+
+    for sublist in matrix:
+        if not isinstance(sublist, (list)):
+            raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+        for element in sublist:
+            if type(element) != int and type(element) != float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+
+    row_size = len(matrix[0])
+    if len(matrix) > 1:
+        for row in matrix[1:]:
+            if len(row) != row_size:
+                raise TypeError("Each row of the matrix must \
+have the same size")
 
     for sublist in matrix:
         for element in sublist:
