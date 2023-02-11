810a70fe80fc55ef5e3a1d3dc931b81e01fe0a50
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-11 17:10:22 -0500

    Prints a text with 2 new lines after specific symbols

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This module defines the function text_indentation """
+
+
+def text_indentation(text):
+    """ Prints a text with 2 new lines after each of these characters:
+        '.', '?', and ':'
+
+        Arguments:
+            text (str): text to be printed
+
+        Returns:
+            nothing
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    symbols = ('.', '?', ':')
+    symbol_printed = False
+    for letters in range(len(text)):
+        letter = text[letters]
+        if letters and letter == " " and symbol_printed:
+            continue
+
+        if letter in symbols:
+            print("{}\n".format(letter))
+            symbol_printed = True
+        else:
+            print(letter, end="")
+            symbol_printed = False
