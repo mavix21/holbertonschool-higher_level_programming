606345834a878f78270a0af86ac27131ea52a778
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-15 00:31:41 -0500

    Checks if an object class inherited, directly or indirectly, from a specified class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -21,4 +21,5 @@ def inherits_from(obj, a_class):
         False otherwise
 
     """
-    return issubclass(obj.__class__, a_class)
+    return issubclass(obj.__class__, a_class) \
+        and obj.__class__ is not a_class
