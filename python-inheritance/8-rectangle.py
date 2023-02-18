0fa4cdb0a770ad253d9d3a24eff790b400fae064
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-18 14:20:55 -0500

    Fix: Rectangle must inherits from existing module

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
-"""This module defines the class BaseGeometry"""
-
-
-class BaseGeometry:
-    """BaseGeometry class"""
-
-    def area(self):
-        """ Raises an Exception """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Validates value """
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""This module defines the class Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
