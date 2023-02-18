701a5c6dcd8c9aaf35ad6e5e6c6467acef0fd1f4
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-18 15:06:39 -0500

    integer_validator mehtod must be called using self

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """ Instantation method """
 
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
