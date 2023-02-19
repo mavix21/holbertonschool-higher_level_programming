1850917b1c7be6d023810cec39096ee4e2bcaaf7
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-19 15:57:07 -0500

    Defines a class Rectangle that inherits from Base

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" This module defines the Rectangle class """
+from base import Base
+
+
+class Rectangle(Base):
+    """
+    This class is the "base" of all other classes in this project. The goal of
+    it is to manage id attribute in all other classes and to avoid duplicating
+    the same code
+
+    Private Class Attributes:
+        nb_objects (int):
+            Incremented when a new object is instanziated
+
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor method """
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Getter method for width property """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter method for width property """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter method for height property """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter method for height property """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter method for x property """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter method for x property """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter method for y property """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter method for y property """
+        self.__y = value
