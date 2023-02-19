13a6c0e9cae786fdae9de948a130ab0bbb80463d
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-19 16:04:41 -0500

    Fix documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """ This module defines the Rectangle class """
+
 from base import Base
 
 
 class Rectangle(Base):
     """
-    This class is the "base" of all other classes in this project. The goal of
-    it is to manage id attribute in all other classes and to avoid duplicating
-    the same code
+    Rectangle class
 
-    Private Class Attributes:
-        nb_objects (int):
-            Incremented when a new object is instanziated
+    Private Instance Attributes:
+        width (int):
+            width of the rectangle instance
+        heigth (int):
+            height of the rectangle instance
+        x (int):
+            x position of the rectangle instance
+        y (int):
+            y position of the rectangle instance
+        id (int):
+            unique identifier for the rectangle instance
 
     """
 
@@ -27,39 +34,47 @@ class Rectangle(Base):
     @property
     def width(self):
         """ Getter method for width property """
+
         return self.__width
 
     @width.setter
     def width(self, value):
         """ Setter method for width property """
+
         self.__width = value
 
     @property
     def height(self):
         """ Getter method for height property """
+
         return self.__height
 
     @height.setter
     def height(self, value):
         """ Setter method for height property """
+
         self.__height = value
 
     @property
     def x(self):
         """ Getter method for x property """
+
         return self.__x
 
     @x.setter
     def x(self, value):
         """ Setter method for x property """
+
         self.__x = value
 
     @property
     def y(self):
         """ Getter method for y property """
+
         return self.__y
 
     @y.setter
     def y(self, value):
         """ Setter method for y property """
+
         self.__y = value
