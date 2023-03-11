5c4116c136b11e0bffeeff2b862039325599bfed
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-11 14:23:31 -0500

    Add comment with alternative way of solving the task

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,9 +2,15 @@
 SELECT
     tv_shows.title,
     tv_genres.name
+-- Another way:
+-- FROM tv_genres
+-- INNER JOIN tv_show_genres
+-- ON tv_genres.id = tv_show_genres.genre_id
+-- RIGHT OUTER JOIN tv_shows
+-- ON tv_show_genres.show_id = tv_shows.id
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
-    ON tv_show_genres.show_id = tv_shows.id
+    ON tv_shows.id = tv_show_genres.show_id
 LEFT OUTER JOIN tv_genres
     ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name
