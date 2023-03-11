aa59de80b951feed2fdaac15e92ebf186f78961d
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-10 23:03:32 -0500

    Fix: Do not hard-code the databse, it will be passed as argument

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,9 @@
--- Lists all the cities of California that can be found in the database
--- `hbtn_0d_usa`
+-- Lists all the cities of California that can be found in a given database
 SELECT
     id,
     name
-FROM hbtn_0d_usa.cities
+FROM cities
 WHERE state_id = (
         SELECT id
-        FROM hbtn_0d_usa.states
+        FROM states
         WHERE name = 'California')
