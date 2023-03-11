e01d2e1c533ad9119e6e3b9c9b799298543bf79a
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-10 21:17:10 -0500

    Fix syntaxis

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates the user `user_0d_1 with all privileges`
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY PASSWORD 'user_0d_1_pwd';
+IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
