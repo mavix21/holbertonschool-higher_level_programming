4045603deca4038950586d1e74296897f9985ed6
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-10 22:33:39 -0500

    Creates the table cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- Creates the database `hbtn_0d_usa` and the table `states`
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    state_id VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states (id)
+);
