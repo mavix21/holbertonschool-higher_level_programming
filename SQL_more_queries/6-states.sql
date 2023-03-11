7850c15da4e73a69a27a5cd284ac15dbc2a5d25a
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-10 22:14:49 -0500

    Fix: must specified in which database the table will be created

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creates the database `hbtn_0d_usa` and the table `states`
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT NOT NULL UNIQUE AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
