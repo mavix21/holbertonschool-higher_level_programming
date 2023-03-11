696f2507aafb42efe1a23f11d407cd70ba143f07
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-10 22:37:47 -0500

    Add name field

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,9 @@
 -- Creates the database `hbtn_0d_usa` and the table `states`
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-    state_id VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states (id)
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
