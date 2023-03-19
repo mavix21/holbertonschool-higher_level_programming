62e91d2ed169ea7adb3471970cdb5868068e8dab
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-18 22:40:25 -0500

    Improvement for model_city module

---
+++ python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Defines the class City """
+
+from sqlalchemy import ForeignKey, create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+import sys
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ State class that links to MySQL table cities """
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True, unique=True, primary_key=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+
+if __name__ == "__main__":
+    av = sys.argv
+    conn = 'mysql+mysqldb://{}:{}@localhost/{}'.format(av[1], av[2], av[3])
+    engine = create_engine(conn, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
