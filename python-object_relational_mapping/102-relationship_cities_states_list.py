44e248c3fec62a44189fbd4ba185eddde74f48f6
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-18 22:58:59 -0500

    List all Cities and their corresponding State

---
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+    Adds the State object "California" (row) with the City "San Francisco"
+    to states table
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_city import Base, City
+    from relationship_state import State
+
+    av = sys.argv
+    try:
+        conn = 'mysql+mysqldb://{}:{}@localhost/{}'.format(av[1], av[2], av[3])
+        engine = create_engine(conn)
+
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        try:
+            for city in session.query(City).order_by(City.id).all():
+                print("{}: {} -> {}".format(city.id, city.name,
+                                            city.state.name))
+
+        except Exception as e:
+            print("Error: {}".format(str(e)))
+
+        finally:
+            session.close()
+
+    except IndexError:
+        print(f"Usage: {av[0]} <username> <password> <database>")
+
+    except Exception as e:
+        try:
+            msg_0 = str(e).split('\n')[0]
+            msg = msg_0.split(" ", 1)[1].split(",")[1].strip()[1:-2]
+            print("{}: {}".format(e.__class__.__name__, msg))
+
+        except Exception:
+            print("Error: {}".format(str(e)))
