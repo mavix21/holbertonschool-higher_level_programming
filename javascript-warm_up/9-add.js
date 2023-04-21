bd5666e66012c2b69fa933dcccd172f2791280a4
mavix21 <marcevizcarra2109@hotmail.com>
2023-04-20 23:20:48 -0500

    add function

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const arg1 = +argv[2];
+const arg2 = +argv[3];
+
+function add(a, b) {
+  let sum = a + b;
+  console.log(sum);
+}
+
+add(arg1, arg2);
