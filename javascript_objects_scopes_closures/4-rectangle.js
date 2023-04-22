e1b4378673c6eda33285322e951527f77d13757c
mavix21 <marcevizcarra2109@hotmail.com>
2023-04-22 14:34:12 -0500

    Added methods rotate and double

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const repr = [];
+    for (let i = 0; i < this.height; i++) {
+      repr.push('X'.repeat(this.width));
+    }
+
+    console.log(repr.join('\n'));
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
