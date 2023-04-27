4d33f69e3e208fc08cd771a289feb2aca1d89567
mavix21 <marcevizcarra2109@hotmail.com>
2023-04-26 23:25:16 -0500

    Writes content to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const fileContent = process.argv[3];
+
+if (!filePath) {
+  console.log('Please specify a file path');
+  process.exit(1);
+}
+
+if (!fileContent) {
+  console.log('Please specify the content to be writen');
+  process.exit(1);
+}
+
+fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
