58e184ad3fa5135fa28fbda4fbf4940d7a1083ed
mavix21 <marcevizcarra2109@hotmail.com>
2023-04-30 15:17:07 -0500

    Use require

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,27 @@
 #!/usr/bin/node
-const axios = require('axios').default;
 
+const request = require('request');
 const url = process.argv[2];
+const characterId = '18';
+
 if (!url) {
   console.log('Please, provide an url');
   process.exit(1);
 }
 
-axios.get(url)
-  .then(({ data: { results } }) => {
-    let count = 0;
-    results.forEach(({ characters }) => {
-      characters.forEach((url) => {
-        if (url.includes('18')) count++;
-      });
-    });
-    console.log(count);
-  })
-  .catch(err => console.error(err));
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  let counter = 0;
+  const filmObjects = JSON.parse(body).results; // array of film objects
+  for (const filmObj of filmObjects) {
+    for (const url of filmObj.characters) {
+      if (url.includes(characterId)) counter++;
+    }
+  }
+
+  console.log(counter);
+});
