e2a0586c18d59bb2c1becfd12abf188740c4ed78
mavix21 <marcevizcarra2109@hotmail.com>
2023-04-30 15:08:10 -0500

    Use axios

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,51 +1,20 @@
 #!/usr/bin/node
+const axios = require('axios').default;
 
-const request = require('request');
-const mainUrl = process.argv[2];
-
-if (!mainUrl) {
+const url = process.argv[2];
+if (!url) {
   console.log('Please, provide an url');
   process.exit(1);
 }
 
-async function getCharacterName (url) {
-  return new Promise((resolve, reject) => {
-    request.get(url, (error, response, body) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-        return;
-      }
-
-      const characterName = JSON.parse(body).name;
-      resolve(characterName);
+axios.get(url)
+  .then(({ data: { results } }) => {
+    let count = 0;
+    results.forEach(({ characters }) => {
+      characters.forEach((url) => {
+        if (url.includes('18')) count++;
+      });
     });
-  });
-}
-
-request.get(mainUrl, async (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const filmObjects = JSON.parse(body).results; // array of film objects
-  let count = 0;
-  const characterNamePromises = [];
-
-  for (const filmObj of filmObjects) {
-    for (const url of filmObj.characters) {
-      characterNamePromises.push(getCharacterName(url));
-    }
-  }
-
-  const characterNames = await Promise.all(characterNamePromises);
-
-  for (const characterName of characterNames) {
-    if (characterName === 'Wedge Antilles') {
-      count++;
-    }
-  }
-
-  console.log(count);
-});
+    console.log(count);
+  })
+  .catch(err => console.error(err));
